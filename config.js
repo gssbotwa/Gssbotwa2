0609a24e34837536baf5a408a13ec21cf9990923
Itx_manno_05 <149866936+Itxmanno05@users.noreply.github.com>
2024-04-03 14:51:53 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,17 @@ const chalk = require('chalk');
 
 
 // Other
-global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.link = 'https://chat.whatsapp.com/JLYhBR3rZPC00zyBHMyqDL'
+global.linkGroup = 'https://chat.whatsapp.com/JLYhBR3rZPC00zyBHMyqDL'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
-global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.ownername = "*⏜✩ ˻𝐈ʈͥ᥊ ϻaͣŋͫŋ̑ο̑˼ 𓃮•:)*";
+global.owner = ['923097045246'];
+global.premium = ['923097045246'];
+global.packname = "⏜✩ ˻₀̄₃̄ͥ₀̄ͭ₉̄ͯ₇̄₀̄ᷟ₄̄ᷧ₅̄ᷡ₂̄ᷳ₄̄ᷳ₆̄˼ 𓃮•:)                  
+
+🌸🤭- " 𝐀 𝐅ɑkə 𝐒mıɭə 𝐂ɑη ┣𝐥ıdə  𝐌ıɭɭıoηs 𝐎f 𝐓əɑrs ||%❤😚";
+global.author = ".";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
@@ -19,13 +21,13 @@ global.menuType = "2"
 global.autoswview = true
 global.autoread = false
 global.available = false
-global.autoTyping = false
+global.autoTyping = true
 global.autoRecord = false
-global.autoreact = false
+global.autoreact = true
 global.onlygroup = false
 global.onlypc = false
-global.welcome = true
-global.autoBlock = true
+global.welcome = false
+global.autoBlock = falde
 
 /*REPLY MESSAGE*/
 global.mess = {
@@ -34,11 +36,11 @@ global.mess = {
     success: '`✓ Success`',
     admin: '`This feature is only for group admins`',
     botAdmin: '`I am not an admin!`',
-    owner: '`You are not my owner`',
+    owner: '`You are not my owner my owner MANNO-2.0`',
     group: '`You can use this command only in groups ❌`',
     private: '`Feature is used only for private chats!`',
     bot: '`Bot number user special features`',
-    wait: '*Processing Your request*',
+    wait: '*Processing Your request by manno.-2.0*',
     endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
 };
 
