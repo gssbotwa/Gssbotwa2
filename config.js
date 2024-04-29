363cdc98d765e97f0e2216fb48972e8ca811eb4a
newbot1234 <168170282+newbot1234@users.noreply.github.com>
2024-04-29 07:05:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "𝐑𝐀𝐒𝐈𝐘𝐀999";
+global.owner = ['94753721749'];
+global.premium = ['94753721749'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.author = "𝐑𝐀𝐒𝐈𝐘𝐀999-𝚩𝚯𝚻𝐖𝚫";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
 global.menuType = "2"
-global.autoswview = true
+global.autoswview = false
 global.autoread = false
 global.available = false
 global.autoTyping = false
@@ -24,8 +24,8 @@ global.autoRecord = false
 global.autoreact = false
 global.onlygroup = false
 global.onlypc = false
-global.welcome = true
-global.autoBlock = true
+global.welcome = false
+global.autoBlock = false
  
 /*REPLY MESSAGE*/
 global.mess = {
